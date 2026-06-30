c41492e707ec4b914c35f995a4222eec5c8d70f6
Flagelo6482 <frankln6482@gmail.com>
2026-06-30 14:46:24 -0500

    feat: nuevos cambios

--- Asistente/scripts/instalar_voces.bat
+++ Asistente/scripts/instalar_voces.bat
@@ -5,7 +5,7 @@ echo ================================================
 echo.
 
 echo [1/2] Activando entorno virtual...
-call frank_env\Scripts\activate
+call ..\frank_env\Scripts\activate
 
 echo [2/2] Instalando edge-tts y playsound...
 pip install edge-tts playsound
